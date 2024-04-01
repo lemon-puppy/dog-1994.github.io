421d88bebb1f092997ae8a3f3b782c7673388e1c
lemon <2455829908@qq.com>
2024-04-01 09:24:24 +0800

    Add files via upload

--- pwa/sw.js
+++ pwa/sw.js
@@ -1,10 +1,10 @@
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.1.0/workbox-sw.js");
-var cacheStorageKey = 'minimal-pwa-1'
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js");
+var cacheStorageKey = 'minimal-pwa-2'
 var cacheList=[
   '/',
   'index.html',
   'main.css',
-  'youhun.jpg'
+  'icon.png'
 ]
 self.addEventListener('install',e =>{
   e.waitUntil(
