6af2d7ace43afe83d57bc4394bf4d69b9f900a3e
lemon <4841650+lemon-puppy@user.noreply.gitee.com>
2024-08-06 17:06:26 +0800

    fix beast bug

---
+++ beast_demo/src/settings.8b013.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/start/start.fire",orientation:"portrait",jsList:[],bundleVers:{internal:"d0832",module_beast:"ef866",module_escape:"dd230",resources:"d5a20",audio:"26f44",main:"4a574"}};
