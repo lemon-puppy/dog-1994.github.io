b18159528e0c5212c9f6ea295c1d629322f81400
lemon <4841650+lemon-puppy@user.noreply.gitee.com>
2024-08-16 11:45:48 +0800

    update test/app

--- test/app.js
+++ test/app.js
@@ -1,26 +1,4 @@
 var deferredPrompt;
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('service-worker.js')
-    .then(function (registration) {
-        console.log('Service Worker registered with scope:', registration.scope);
-
-        registration.addEventListener('updatefound', () => {
-            const installingWorker = registration.installing;
-            installingWorker.onstatechange = () => {
-                if (installingWorker.state === 'installed') {
-                    if (navigator.serviceWorker.controller) {
-                        // 有更新的内容
-                        if (confirm("新版本可用，是否刷新页面以获取最新内容？")) {
-                            window.location.reload();
-                        }
-                    }
-                }
-            };
-        });
-    }).catch(function (error) {
-        console.log('Service Worker registration failed:', error);
-    });
-}
 
 window.addEventListener('beforeinstallprompt', (e) => {
     e.preventDefault();
@@ -30,7 +8,7 @@ window.addEventListener('beforeinstallprompt', (e) => {
 
 document.getElementById('install-button').addEventListener('click', () => {
     console.log(deferredPrompt);
-    alert("123"+deferredPrompt);
+    alert("123" + deferredPrompt);
     if (deferredPrompt) {
         deferredPrompt.prompt();
         deferredPrompt.userChoice.then((choiceResult) => {
@@ -40,7 +18,30 @@ document.getElementById('install-button').addEventListener('click', () => {
                 console.log('用户取消了安装');
             }
             deferredPrompt = null;
-            alert("456"+deferredPrompt);
+            alert("456" + deferredPrompt);
         });
     }
 });
+
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('service-worker.js')
+        .then(function (registration) {
+            console.log('Service Worker registered with scope:', registration.scope);
+            registration.addEventListener('updatefound', () => {
+                const installingWorker = registration.installing;
+                installingWorker.onstatechange = () => {
+                    if (installingWorker.state === 'installed') {
+                        if (navigator.serviceWorker.controller) {
+                            // 有更新的内容
+                            if (confirm("新版本可用，是否刷新页面以获取最新内容？")) {
+                                window.location.reload();
+                            }
+                        }
+                    }
+                };
+            });
+        }).catch(function (error) {
+            console.log('Service Worker registration failed:', error);
+        });
+}
+
