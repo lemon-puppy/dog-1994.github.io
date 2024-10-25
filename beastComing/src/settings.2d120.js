b3b9ed1d6b9e6d82e5fde2b376e73d8e1fcf6c74
lemon <4841650+lemon-puppy@user.noreply.gitee.com>
2024-10-25 11:09:56 +0800

    upload game beastComing

---
+++ beastComing/src/settings.2d120.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/start/start.fire",orientation:"portrait",jsList:[],bundleVers:{internal:"d0832",resources:"26629",audio:"bec92",prefabs:"bebfb",main:"9b672"}};
